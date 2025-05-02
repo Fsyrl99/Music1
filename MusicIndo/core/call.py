7db221c84ad912e09f6b134580e2caf324e3654f
Fsyrl99 <118256932+Fsyrl99@users.noreply.github.com>
2025-05-02 14:09:19 +0700

    Update call.py

--- MusicIndo/core/call.py
+++ MusicIndo/core/call.py
@@ -24,6 +24,7 @@ from pytgcalls import PyTgCalls, filters
 from pytgcalls.exceptions import *
 from pytgcalls.types import *
 from pytgcalls.types.stream import *
+from pytgcalls.types import JoinedGroupCallParticipant, LeftGroupCallParticipant
 
 import config
 from strings import get_string
