b833f6cca61ededc59d60ae5efbcf45dba1da916
Fsyrl99 <118256932+Fsyrl99@users.noreply.github.com>
2025-05-02 14:04:55 +0700

    Update call.py

--- MusicIndo/core/call.py
+++ MusicIndo/core/call.py
@@ -24,6 +24,7 @@ from pytgcalls import PyTgCalls, filters
 from pytgcalls.exceptions import *
 from pytgcalls.types import *
 from pytgcalls.types.stream import *
+from pytgcalls.types.update import UpdatedGroupCallParticipant
 
 import config
 from strings import get_string
@@ -674,7 +675,7 @@ class Call(PyTgCalls):
         @self.four.on_update(filters.call_participant(GroupCallParticipant.Action.UPDATED))
         @self.five.on_update(filters.call_participant(GroupCallParticipant.Action.UPDATED))
         async def participants_change_handler(client, update: Update):
-            if not isinstance(update, JoinedGroupCallParticipant) and not isinstance(
+            if not isinstance(update, UpdatedGroupCallParticipant) and not isinstance(
                 update, LeftGroupCallParticipant
             ):
                 return
